20efd434326e4fb2ad9f1d874805b3379b320459
uness01 <boumedianiyounes6@gmail.com>
2023-02-25 15:02:06 +0100

    scanner

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("world");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("what is your name? ");
+		String name =scanner.nextLine();
+		
+		System.out.println("How old are you? ");
+		int age=scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("what is your favorite food? ");
+		String food =scanner.nextLine();
+		
+		System.out.println("Hello "+name);
+		System.out.println("you are "+age+" years old");
+		System.out.println("your favorite food is "+food);
 	}
 
 }
